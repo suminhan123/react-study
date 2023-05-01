e51410571f1f7d75386842933adab7d55e4e8167
suminhan123 <suminh324@gmail.com>
2023-05-01 18:09:13 +0900

    chap18-리덕스 라이브러리 없이 커스텀 훅 store 구현

---
+++ chap18-물품 즐겨 찾기/src/hooks-store/store.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+
+// state를 전역적으로 사용하는 솔루션
+let globalState = {};
+let listeners = [];
+let actions = {};
+
+export const useStore = (shouldListen = true) => {
+  const setState = useState(globalState)[1];
+
+  const dispatch = (actionIdentifier, payload) => {
+    const newState = actions[actionIdentifier](globalState, payload);
+    globalState = { ...globalState, ...newState };
+  };
+  for (const listener of listeners) {
+    listener(globalState);
+  }
+
+  useEffect(() => {
+    if (shouldListen) {
+      listeners.push(setState);
+    }
+    return () => {
+      if (shouldListen) {
+        listeners = listeners.filter((li) => li !== setState);
+      }
+    };
+  }, [setState, shouldListen]);
+  return [globalState, dispatch];
+};
+
+export const initStore = (userActions, initialState) => {
+  if (initialState) {
+    globalState = { ...globalState, ...initialState };
+  }
+  actions = { ...actions, ...userActions };
+};
